e8743e715e776fd1613f827482676415e1e38e93
Emre Akyüz <aemreakyuz@gmail.com>
2023-12-18 22:22:03 +0300

    JPA Intro

---
+++ src/main/java/com/workintech/S18D1ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18D1ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18D1ChallengeApplication.class, args);
+	}
+
+}
